3cff36e1c20b7af43db7d729ab7f1f90a4dbf870
mdsahilhussain <mdsahil.hussain7@gmail.com>
2025-07-17 11:17:36 +0530

    update validation of input, polish the ui like kuvakaa

--- lib/store/useOtpStore.ts
+++ lib/store/useOtpStore.ts
@@ -1,17 +1,27 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
+type User = {
+  phone: string;
+  countryCode: string;
+};
 type OtpStore = {
-  otp: string
-  setOtp: (val: string) => void
-  error: string
-  setError: (val: string) => void
-  clearOtp: () => void
-}
+  user: User;
+  setUser: (val: User) => void;
+  otp: string;
+  setOtp: (val: string) => void;
+  error: string;
+  setError: (val: string) => void;
+  clearOtp: () => void;
+  clearUser: () => void;
+};
 
 export const useOtpStore = create<OtpStore>((set) => ({
-  otp: '',
+  user: { phone: "", countryCode: "" },
+  setUser: (val) => set({ user: val }),
+  otp: "",
   setOtp: (val) => set({ otp: val }),
-  error: '',
+  error: "",
   setError: (val) => set({ error: val }),
-  clearOtp: () => set({ otp: '', error: '' }),
-}))
+  clearOtp: () => set({ otp: "", error: "" }),
+  clearUser: () => set({ user: { phone: "", countryCode: "" } }),
+}));
